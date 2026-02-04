bea0ff31e4f17bd2f3c79b0d764462816adb5823
kaeden <kaeden.j.rhoads@gmail.com>
2026-02-03 20:39:53 -0500

    intake testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,19 +4,26 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.net.WebServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.util.MatchData;
 
 public class Robot extends TimedRobot {
-    private Command autonomousCommand;
+    private Command m_autonomousCommand;
 
     private final RobotContainer robotContainer;
+
     private final MatchData matchData = new MatchData();
+
+    private final SparkMax intake = new SparkMax(5, SparkMax.MotorType.kBrushless);
+
+    private final Joystick intakeJoystick = new Joystick(1);
 
     public Robot() {
         // Silence joystick warnings in development (overridden when connected to FMS)
@@ -33,7 +40,6 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotPeriodic() {
-        matchData.periodic();
         CommandScheduler.getInstance().run();
     }
 
@@ -48,10 +54,10 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
-        autonomousCommand = robotContainer.getAutonomousCommand();
+        m_autonomousCommand = robotContainer.getAutonomousCommand();
 
-        if (autonomousCommand != null) {
-            CommandScheduler.getInstance().schedule(autonomousCommand);
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().schedule(m_autonomousCommand);
         }
     }
 
@@ -63,13 +69,25 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        if (autonomousCommand != null) {
-            autonomousCommand.cancel();
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().cancel(m_autonomousCommand);
         }
     }
 
     @Override
-    public void teleopPeriodic() {}
+    public void teleopPeriodic() {
+
+        // intake test
+        if (intakeJoystick.getRawButton(4)) {
+            intake.set(1);
+            System.out.println("intake forward");
+        } else if (intakeJoystick.getRawButton(1)) {
+            intake.set(-1);
+            System.out.println("intake reverse");
+        } else {
+            intake.set(0.0);
+        }
+    }
 
     @Override
     public void teleopExit() {}
@@ -84,4 +102,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void testExit() {}
+
+    @Override
+    public void simulationPeriodic() {}
 }
