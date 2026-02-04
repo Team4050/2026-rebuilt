4ff296a19588ba66958b89c5c9b8903af1b1d5e1
Carson Seese <carson@carsonseese.com>
2026-02-04 08:44:29 -0500

    Set up constants files

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "2026-rebuilt";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 29;
+    public static final String GIT_SHA = "f72051716709c29650fccbc1a7b81f227db7179f";
+    public static final String GIT_DATE = "2026-02-04 08:05:54 EST";
+    public static final String GIT_BRANCH = "Implement-AdvantageKit";
+    public static final String BUILD_DATE = "2026-02-04 08:40:15 EST";
+    public static final long BUILD_UNIX_TIME = 1770212415541L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
