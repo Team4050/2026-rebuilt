37fe26675b6e414978d7404ffad0c34f68b8c28e
kaeden <kaeden.j.rhoads@gmail.com>
2026-02-17 19:12:01 -0500

    add config to intke

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,18 +1,34 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.REVLibError;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
 
     private final SparkMax intake = new SparkMax(Constants.Subsystems.intakeRollerId, SparkMax.MotorType.kBrushless);
-    private final SparkMax intakeDeploy =
-            new SparkMax(Constants.Subsystems.intakeDeployId, SparkMax.MotorType.kBrushless);
+    // private final SparkMax intakeDeploy = new SparkMax(Constants.Subsystems.intakeDeployId, SparkMax.MotorType.kBrushless);
+
+    public Intake() {
+        SparkMaxConfig mainConfig = new SparkMaxConfig();
+
+        mainConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40);
+
+        if (intake.configure(mainConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters)
+                != REVLibError.kOk) {
+            throw new IllegalStateException("Error configuring Intake Motor");
+        }
+    }
 
     public void stop() {
         intakeStop();
-        deployStop();
+        // deployStop();
     }
 
     public void intakeForward() {
@@ -27,15 +43,15 @@ public class Intake extends SubsystemBase {
         intake.set(-1);
     }
 
-    public void deployOut() {
-        intakeDeploy.set(0.5);
-    }
+    // public void deployOut() {
+    //     intakeDeploy.set(0.5);
+    // }
 
-    public void deployStop() {
-        intakeDeploy.set(0);
-    }
+    // public void deployStop() {
+    //     intakeDeploy.set(0);
+    // }
 
-    public void deployIn() {
-        intakeDeploy.set(-0.5);
-    }
+    // public void deployIn() {
+    //     intakeDeploy.set(-0.5);
+    // }
 }
