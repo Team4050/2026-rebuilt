8f5da7c09423e4e8e8ef9a4e83ca6c7c5b652ab5
Carson Seese <carson@carsonseese.com>
2026-02-18 19:36:10 -0500

    Fix CAN IDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,21 +13,25 @@ public final class Constants {
         // CAN device IDs - Schema is <module number: 1-4><device: drive = 1, steer = 2, encoder = 3>
         // =========================================================================================================================
 
+        // Front left - Module 1
         public static final int frontLeftDriveId = 11;
         public static final int frontLeftSteerId = 12;
         public static final int frontLeftEncoderId = 13;
 
-        public static final int frontRightDriveId = 21;
-        public static final int frontRightSteerId = 22;
-        public static final int frontRightEncoderId = 23;
+        // Front right - Module 4
+        public static final int frontRightDriveId = 41;
+        public static final int frontRightSteerId = 42;
+        public static final int frontRightEncoderId = 43;
 
-        public static final int backLeftDriveId = 31;
-        public static final int backLeftSteerId = 32;
-        public static final int backLeftEncoderId = 33;
+        // Back left - Module 2
+        public static final int backLeftDriveId = 21;
+        public static final int backLeftSteerId = 22;
+        public static final int backLeftEncoderId = 23;
 
-        public static final int backRightDriveId = 41;
-        public static final int backRightSteerId = 42;
-        public static final int backRightEncoderId = 43;
+        // Back right - Module 3
+        public static final int backRightDriveId = 31;
+        public static final int backRightSteerId = 32;
+        public static final int backRightEncoderId = 33;
     }
 
     public final class Subsystems {
