6491d61a8ec9381483e720b2efed94104454e663
Carson Seese <carson@carsonseese.com>
2026-02-22 16:10:45 -0500

    Move BuildConstants

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.generated.BuildConstants;
 import frc.robot.util.Elastic;
 
 @Logged
