0cf5e1e3615506adbd05531665b64abde5be7227
kaeden <kaeden.j.rhoads@gmail.com>
2026-03-07 13:04:57 -0500

    Out take fowerd and revse set up.
    Co-authored-by: tirhoads <tirhoads@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Outake.java
+++ src/main/java/frc/robot/subsystems/Outake.java
@@ -1,38 +1,48 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.REVLibError;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Outake extends SubsystemBase {
-  // private final SparkMax motor = new SparkMax(Constants.Subsystems.outakeRollerId, SparkMax.MotorType.kBrushless);
+  private final SparkMax motor = new SparkMax(Constants.Subsystems.outakeRollerId, SparkMax.MotorType.kBrushless);
 
-  //   private final double speed = 0.7;
+  private final double speed = 0.7;
 
-  //   public OutakeSub() {
-  //     final SparkMaxConfig config = new SparkMaxConfig();
-  //     config.idleMode(IdleMode.kCoast).smartCurrentLimit(50);
+  public Outake() {
+    final SparkMaxConfig config = new SparkMaxConfig();
+    config.idleMode(IdleMode.kCoast).smartCurrentLimit(50);
 
-  //     if (motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters) != REVLibError.kOk) {
-  //       DriverStation.reportWarning("Error configuring Intake Rollers Motor", false);
-  //     }
-  //   }
+    if (motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters) != REVLibError.kOk) {
+      DriverStation.reportWarning("Error configuring Outake Motor", false);
+    }
+  }
 
-  //   private void stop() {
-  //     motor.stopMotor();
-  //   }
+  private void stop() {
+    motor.stopMotor();
+  }
 
-  //   public Command stopCommand() {
-  //     return runOnce(this::stop).withName("Intake Rollers: Stop");
-  //   }
+  public Command stopCommand() {
+    return runOnce(this::stop).withName("Outtake: Stop");
+  }
 
-  //   public void intakeForward() {
-  //     motor.set(speed);
-  //   }
+  public void Forward() {
+    motor.set(speed);
+  }
 
-  //   public void intakeReverse() {
-  //     motor.set(-speed);
-  //   }
+  public void Reverse() {
+    motor.set(-speed);
+  }
 
-  //   public double motorCurrent() {
-  //     return motor.getOutputCurrent();
-  //   }
+  public double motorCurrent() {
+    return motor.getOutputCurrent();
+  }
 }
