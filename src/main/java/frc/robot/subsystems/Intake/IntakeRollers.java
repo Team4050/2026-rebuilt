94b5fbaf2ad9aa14d2423afd35f7c7cf7837dfbf
Carson Seese <carson@carsonseese.com>
2026-03-14 09:10:38 -0400

    Add configurable intake speed

--- src/main/java/frc/robot/subsystems/Intake/IntakeRollers.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeRollers.java
@@ -8,17 +8,21 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IntakeRollers extends SubsystemBase {
 
-  private final double SPEED = 0.6;
+  private static final String SPEED_KEY = "Intake Roller Speed";
+  private static final double DEFAULT_SPEED = 0.55;
 
   private final SparkMax motor = new SparkMax(Constants.Subsystems.intakeRollerId, SparkMax.MotorType.kBrushless);
 
   public IntakeRollers() {
+    SmartDashboard.putNumber(SPEED_KEY, DEFAULT_SPEED);
+
     var config = new SparkMaxConfig();
     config.idleMode(IdleMode.kCoast).smartCurrentLimit(50);
 
@@ -27,16 +31,20 @@ public class IntakeRollers extends SubsystemBase {
     }
   }
 
+  private double getSpeed() {
+    return SmartDashboard.getNumber(SPEED_KEY, DEFAULT_SPEED);
+  }
+
   private void stop() {
     motor.stopMotor();
   }
 
   private void intakeIn() {
-    motor.set(SPEED);
+    motor.set(getSpeed());
   }
 
   private void intakeOut() {
-    motor.set(-SPEED);
+    motor.set(-getSpeed());
   }
 
   public Command inCommand() {
@@ -45,9 +53,5 @@ public class IntakeRollers extends SubsystemBase {
 
   public Command outCommand() {
     return startEnd(this::intakeOut, this::stop).withName("Intake: Out");
-  }
-
-  public double motorCurrent() {
-    return motor.getOutputCurrent();
   }
 }
